664c483dea631c61b1d02571529032c69bba2b4a
appsconda <145383447+appsconda@users.noreply.github.com>
2023-09-30 23:40:08 +0200

    Create appscondacss.php

---
+++ appscondacss.php
@@ -0,0 +1,38 @@
+<?php
+/*
+	Copyright (C) 2023 AppsConda Limited. All Rights Reserved.
+	License: GNU General Public License version 2 or later;
+	Author: appsconda.com
+	Website: https://appsconda.com
+	Modified: 09/30/2023
+	------------------------------------------------------------------------
+*/
+
+defined('_JEXEC') or die('No direct access');
+
+jimport('joomla.plugin.plugin');
+
+use Joomla\CMS\Factory;
+
+
+
+class plgSystemAppscondacss extends JPlugin{
+	function onBeforeCompileHead() {
+
+		$doc = Factory::getDocument();
+		$app = Factory::getApplication();
+		$is_site = $app->getName() === 'site';
+		
+		if ($is_site) {
+		    
+			$appscondacss = $this->params->get('appscondacss');
+				if (strlen($appscondacss) > 0) {
+				$doc->addCustomTag('<style type="text/css">'.$appscondacss.'</style>');
+				}
+		}
+
+		
+
+	
+	}
+}
